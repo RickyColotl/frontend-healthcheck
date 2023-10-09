6e7922ec4186ef4e9e6a69a2f473a8c08fbe642c
RickyColotl <144842683+RickyColotl@users.noreply.github.com>
2023-10-09 00:52:44 -0400

    Add Rent Out feature for Movie Page

--- angular-frontend/src/app/movies/movies.component.ts
+++ angular-frontend/src/app/movies/movies.component.ts
@@ -23,7 +23,27 @@ export class MoviesComponent {
       }
     );
   }
+  
   selectMovie(movie: any) {
     this.selectedMovie = this.selectedMovie === movie ? null : movie;
   }
+
+  rentMovie(filmId: number) {
+    const customerId = Number(prompt("Please enter your customer ID:"));
+    if (isNaN(customerId)) {
+     alert("Invalid customer ID!");
+     return;
+    }
+    const url = `http://localhost:3000/rentMovie?filmId=${filmId}&customerId=${customerId}`;
+    console.log("Request URL:", url);
+    this.http.get<any>(url).subscribe(
+      (response) => {
+        alert(`Movie rented successfully on ${response.rental.rentalDate}!`);
+      },  
+      (error) => {
+        alert("Error renting the movie!");
+      }
+    );
+ }
+
 }
